b6ad7dd391e78a092d07397348e77a1a72ada9bb
Harsh2417 <65455754+Harsh2417@users.noreply.github.com>
2023-06-07 13:31:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+import About from './Component1/about';
+import Educator from './Component1/educator';
+import Expierence from './Component1/experience';
+import Hobbies from './Component1/hobbies';
+import Skills from './Component1/skills';
+function App() {
+  return (
+    <div className="App">
+      <About/>
+     <Expierence/>
+     <Educator/>
+     <Skills/>
+     <Hobbies/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component1/about.js
@@ -0,0 +1,13 @@
+const About = _ => {
+
+    return (
+        <>
+        <h1>Kabir Sagar</h1>
+        <h4>Software Developer, Educator at Geekster</h4>
+       <a href="/">LinkedIn</a> &ensp;
+       <a href="/">GitHub</a>
+        
+        </>
+    )
+}
+export default About;
